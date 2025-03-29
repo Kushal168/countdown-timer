17104f29e9fd5b978d66f5da5349e4c848ee617e
Kushal <Kushalgoyal168@gmail.com>
2025-03-29 17:25:04 +0530

    optimise function and value pass

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,26 @@ import './App.css'
 
 function App() {
   const [isStart, setIsStart] = useState(false)
+  const [hour, setHour] = useState(0)
+  const [minute, setMinute] = useState(0)
+  const [second, setSecond] = useState(0)
   function handleClick(){
     setIsStart(true);
+  }
+  function handleHours(e){
+    console.log("--->> ",e.target.name, e.target.value)
+    if(e.target.name == 'hours') setHour(e.target.value);
+    else if(e.target.name == 'minutes') setMinute(e.target.value);
+    else if(e.target.name == 'seconds') setSecond(e.target.value);
   }
 
   return (
     <>
       {!isStart && <div>
         <div>
-          <input className='input_field' type="text" placeholder='HH'/>
-          <input className='input_field' type="text" placeholder='MM'/>
-          <input className='input_field' type="text" placeholder='SS'/>
+          <input className='input_field' type="text" name="hours" onChange = {(e)=>{handleHours(e)}} placeholder='HH'/>
+          <input className='input_field' type="text" name="minutes" onChange = {(e)=>{handleHours(e)}} placeholder='MM'/>
+          <input className='input_field' type="text" name="seconds" onChange = {(e)=>{handleHours(e)}} placeholder='SS'/>
         </div>
         <div>
           <button type='button' onClick={handleClick}> Start </button>
@@ -24,9 +33,9 @@ function App() {
       }
       {isStart && <div>
         <div>
-          <input className='input_field' type="text" placeholder='HH'/>
-          <input className='input_field' type="text" placeholder='MM'/>
-          <input className='input_field' type="text" placeholder='SS'/>
+          <input value={hour} className='input_field' type="text" placeholder='HH'/>
+          <input value={minute} className='input_field' type="text" placeholder='MM'/>
+          <input value={second} className='input_field' type="text" placeholder='SS'/>
         </div>
         <div>
           <button type='button' onClick={handleClick}> Pause </button>
